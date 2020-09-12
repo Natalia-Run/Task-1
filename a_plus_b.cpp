2d4d5a6b3e773253ea5acca0e178e4c32eafc375
Natalia-Run <70914248+Natalia-Run@users.noreply.github.com>
2020-09-12 15:26:44 +0500

    Task-1

---
+++ a_plus_b.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+int a, b, c;
+int main() {
+	cout << " a = ";
+	cin >> a;
+	cout << " b = ";
+	cin >> b;
+	c = a + b;
+	cout << " c = " << c;
+	return 0;
+}
